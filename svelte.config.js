a4e0bee6fbd42cccb1b77c97000612aa8baccc4f
Marco Kellershoff <marco@mistweaverco.com>
2025-12-14 08:56:00 +0100

    chore(shiki): bump version to fix static builds

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import rehypeSlug from 'rehype-slug-custom-id';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import mdsvexShiki from '@mistweaverco/mdsvex-shiki';
+import { mdsvexShiki } from '@mistweaverco/mdsvex-shiki';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
